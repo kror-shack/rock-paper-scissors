27ff5cad1710e9c8720c51637dd64bee330f1852
Fouzan <tariqfouzan1@gmail.com>
2022-06-08 19:44:27 +0000

    first draft

--- external.js
+++ external.js
@@ -1,11 +1,13 @@
 let computerInput;
+const containerTwo = document.querySelector('.containerTwo');
+let userInput;
 let userScore = 0;
 let computerScore = 0;
-let userInput;
 let z=0;
 
-function computerPlay(){
-    let x = Math.floor(Math.random()*3+1);
+
+    function computerPlay(){
+        let x = Math.floor(Math.random()*3+1);
         if(x == 1){
         computerInput = "rock";
          }
@@ -15,61 +17,108 @@ function computerPlay(){
          else if(x == 3){
         computerInput = "scissors"
             }
-    return computerInput;
+        return computerInput;
     }
 
-function oneGame(xe,ye){
-       if ( userInput == computerInput){
-       alert("That is a Draw!")
-       z++;
-       }
-       else if(userInput == "rock" && computerInput == "paper"){
-       alert("The Computer Wins!")
-       computerScore++;
-       z++;
+    function displayUserWin(){
+        const userWins = document.createElement('p');
+        userWins.textContent = "You Win";
+        containerTwo.appendChild(userWins);
+   
+    }
+   
+    function displayComputerWin(){
+       const computerWins = document.createElement('p');
+       computerWins.textContent = "You lose!";
+       containerTwo.appendChild(computerWins);
+   }
+   
+   function displayDraw(){
+       const draw = document.createElement('p');
+       draw.textContent = "This is a draw!";
+       containerTwo.appendChild(draw);
+   }
+
+
+    function checkRound() {
+        if( z == 5){
+             if(userScore > computerScore){
+        const score = document.createElement('p');
+        score.textContent = " Game Over! You Win "  + userScore + "-" + computerScore;
+        containerTwo.appendChild(score);
         }
-       else if(userInput == "scissors" && computerInput == "rock"){
-       alert("The Computer Wins!")
-       computerScore++;
-       z++;
+            else if(computerScore > userScore){
+        
+        const score = document.createElement('p');
+        score.textContent = " Game Over! The computer wins "  + userScore + "-" + computerScore;
+        containerTwo.appendChild(score);
         }
-        else if(userInput == "paper" && computerInput == "rock"){
-        alert("You Win!")
-         userScore++;
-         z++;
+     }
+    }
+
+    function oneGame(xe,ye){
+        if ( userInput == computerInput){
+         displayDraw();
+        }
+         else if(userInput == "paper" && computerInput == "rock"){
+            userScore++;
+            z++;
+            displayUserWin();
+          }
+         else if(userInput == "rock" && computerInput == "scissors"){
+            userScore++;
+            z++;
+            displayUserWin();
          }
-        else if(userInput == "rock" && computerInput == "scissors"){
-        alert("You Win!")
-        userScore++;
-        z++;
-        }
-        else if(userInput == "paper" && computerInput == "scissors"){
-        alert("The Computer Wins!")
-         computerScore++;
-         z++;
-        }
-        else if(userInput == "scissors" && computerInput == "paper"){
-        alert("You Wins!")
+         else if(userInput == "scissors" && computerInput == "paper"){
          userScore++;
          z++;
+         displayUserWin();
+         }
+         else{
+            computerScore++;
+            z++;
+            displayComputerWin();
+         }
+     }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+        userInput = button.id;
+        console.log(userInput);
+        oneGame(userInput, computerPlay());
+        checkRound();
+        if(z == 5){
+        const newGame = document.createElement('button');
+        newGame.textContent = "New Game!";
+        containerTwo.appendChild(newGame);
         }
-    }
- 
-for(let i = 5; i > z;){
-       userInput = prompt("Please choose your weapon:", " ");
-    if(userInput == "rock" || userInput == "paper" || userInput == "scissors"){
-            oneGame(userInput,computerPlay());
-    }
-    else{
-        alert("Please pick a valid weapon: " + "\\n" + "Rock / Paper / Scissors");
-    }
-}
-if(userScore > computerScore){
-        alert("You won " + userScore + "-" + computerScore)
-    }
-else if(computerScore > userScore){
-        alert("The computer wins "  + userScore + "-" + computerScore)
-    }
-else if(computerScore == userScore){
-    alert("It's a draw "  + userScore + "-" + computerScore)
-}
+
+     });
+});
+
+
+
+/*jflkdjkldkdklsfddddddddddddddddddddkjfadlk adjflkasdjf lkdsahf kjhdhk jfdsakfhdaskjfh asdjhf sadh 
+dsa
+
+dasf
+sdf
+sdaf
+f
+sad
+f
+sdafsadf
+a
+fsd
+dfs
+dsa
+fsa
+floordsaf
+sda
+f
+sdaf
+afs*/
+
